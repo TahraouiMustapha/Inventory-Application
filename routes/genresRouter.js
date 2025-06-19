461bea3fa2c18be3ddd4b5364f848a97a3d412ef
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-06-19 19:20:17 +0100

    feat: add route to get form for adding a new genre

--- routes/genresRouter.js
+++ routes/genresRouter.js
@@ -5,6 +5,7 @@ const genresRouter = Router();
 const controller = require('../controllers/genreController')
 
 genresRouter.get("/", controller.getAllGenres )
+genresRouter.get("/addgenre", (req, res)=> res.render("addGenre"))
 genresRouter.get("/:genreId", controller.getMoviesByGenreId )
 
 
