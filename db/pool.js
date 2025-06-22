50b21e717b1520b9b8aaa7e9d39615fc913c037a
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-06-22 19:39:40 +0100

    change pool connection

--- db/pool.js
+++ db/pool.js
@@ -3,9 +3,8 @@ require("dotenv").config();
 
 
 module.exports = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD, 
-  port: process.env.PGPORT
+  connectionString: process.env.DATABASE_URL,
+   ssl: {
+    rejectUnauthorized: false
+  }
 })
