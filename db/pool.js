45f2247f102840a45599139949aadaaa01952a55
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-06-22 19:28:15 +0100

    change the way connecting the postgress db

--- db/pool.js
+++ db/pool.js
@@ -3,8 +3,9 @@ require("dotenv").config();
 
 
 module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL,
-   ssl: {
-    rejectUnauthorized: false
-  }
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD, 
+  port: process.env.PGPORT
 })
